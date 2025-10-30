e217db90fd1948ff56c6eedde322bdc7ae65f085
shannon-lumpkin <sslumpkin@uncg.edu>
2025-10-30 09:31:09 -0400

    stats07 update

---
+++ code/stats07.R
@@ -0,0 +1,142 @@
+pacman::p_load(tidyverse, 
+               patchwork,
+               here)
+ 
+
+# t test vs lm ------------------------------------------------------------
+
+df_fl <- read_csv(here("data_raw/data_fish_length.csv"))
+
+m <- lm(length ~ lake,
+   data = df_fl)
+
+
+# calculate the mean for length -------------------------------------------
+
+v_mu <- df_fl %>% 
+  group_by(lake) %>% 
+  summarize(mu_l = mean(length)) %>% 
+  pull(mu_l)
+
+# mean length for lake "a" ------------------------------------------------
+
+v_mu[1]
+
+# mean length for lake "b" ------------------------------------------------
+
+v_mu[2]
+
+# difference between a & b
+
+v_mu[2] - v_mu[1]
+
+#mean length for lake b
+
+sum(coef(m))
+
+#look into lm result
+summary(m)
+
+#compare with t-test result
+a <- df_fl%>%
+  filter(lake == "a") %>%
+  pull(length)
+b <- df_fl%>%
+  filter(lake == "b") %>%
+  pull(length)
+
+t.test(x =a,
+       y = b,
+       var.equal = TRUE)
+
+
+# anova vs lm -------------------------------------------------------------
+df_anova <- read_csv(here("data_raw/data_fish_length_anova.csv"))
+
+m1 <- lm(length ~ lake,
+   data = df_anova)
+## get the group means
+
+v_mu_anova <-df_anova %>%
+  group_by(lake) %>%
+  summarize(mu_l = mean(length))%>%
+  pull(mu_l)
+
+#this corresponds to "intercept"
+v_mu_anova[1]
+
+#this corresponds to "b"
+v_mu_anova[2] - v_mu_anova[1]
+
+#this corresponds to "lake c effect"
+v_mu_anova[3] - v_mu_anova[1] 
+
+#comparison with aov()
+aov(length ~ lake,
+    data = df_anova)
+
+# ancova ------------------------------------------------------------------
+#ancova example
+m2 <- lm(Sepal.Length ~ Sepal.Width + Species,
+   data = iris)
+
+summary(m2)
+
+# #visualization of ancova example ----------------------------------------
+
+m_iris <- lm(Petal.Length ~ Petal.Width + Species,
+             data = iris)
+
+# create a data frame for prediction
+# variable names must be identical to the original dataframe for analysis
+n_rep <- 100
+df_pred <- tibble(Petal.Width = rep(seq(min(iris$Petal.Width),
+                                        max(iris$Petal.Width),
+                                        length = n_rep),
+                                    n_distinct(iris$Species)),
+                  Species = rep(unique(iris$Species),
+                                each = n_rep))
+
+n_rep <- 100
+df_pred <- tibble(Petal.Width = rep(seq(min(iris$Petal.Width),
+                                        max(iris$Petal.Width),
+                                        length = n_rep),
+                                    n_distinct(iris$Species)),
+                  Species = rep(unique(iris$Species),
+                                each = n_rep))
+
+# get predicted values ----------------------------------------------------
+
+y_pred <- predict(m_iris,
+        newdata = df_pred)
+
+df_pred <- df_pred %>%
+  mutate(y_pred = y_pred)
+
+# get visual output
+
+iris%>%
+  ggplot(aes(x= Petal.Width,
+             y = Petal.Length,
+             color = Species)) +
+  geom_point(alpha = 0.5) +
+  geom_line(data = df_pred,
+            aes(y = y_pred))
+  
+
+# interaction  ------------------------------------------------------------
+
+# how to include interaction
+
+m_int <- lm(Petal.Length ~ Petal.Width * Species,
+   data = iris)
+# identical model with different expression
+lm(Petal.Length ~ Petal.Width + Species + Petal.Width:Species,
+   data = iris)
+summary(m_int)
+
+
+
+
+
+
