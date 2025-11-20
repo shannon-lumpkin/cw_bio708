d8b3acd477f3cc803c15b20f4a67d926c86c2d4d
shannon-lumpkin <sslumpkin@uncg.edu>
2025-11-20 10:45:17 -0500

    extra01_updates

---
+++ code/Extra01.R
@@ -0,0 +1,213 @@
+# Ctrl (Command) + Shift + N is a hotkey for creating a new script file
+# Ctrl + I is to fix indent
+# data cleaning tips in R
+
+pacman::p_load(tidyverse,
+               patchwork,
+               janitor,
+               stringdist,
+               here)
+
+url <- "https://raw.githubusercontent.com/aterui/biostats/master/data_raw/data_messy.csv"
+df_messy <- read_csv(url)
+
+# check data entries before you analyze data ------------------------------
+
+## check class of each column
+## - sapply & class
+
+## check unique elements in each column
+## - sapply & unique
+
+## check possible type
+## - stringdistmatrix()
+
+# text cleaning in R ------------------------------------------------------
+
+## remove white space
+## str_squish()
+
+## align text case
+## str_to_lower() & str_to_upper()
+
+## replace text
+## str_replace() & str_replace_all()
+
+## remove text
+## str_remove() & str_remove_all()
+
+## detect text
+## str_detect()
+
+# column manipulation based on column type --------------------------------
+## mutate(), across(), where()
+
+# example code ------------------------------------------------------------
+
+# chr_clean <- function(x) {
+#   x %>%
+#     str_squish() %>% 
+#     str_to_lower() %>% 
+#     str_replace_all("\\.|\\s", "_") %>% 
+#     str_remove_all("^_|_$")
+# }
+# 
+# df_messy %>% 
+#   mutate(collector = chr_clean(collector),
+#          species = chr_clean(species),
+#          length_mm = str_squish(length_mm) %>% 
+#            str_replace(",", "\\.") %>% 
+#            str_extract("\\d{1,}") %>% 
+#            as.numeric(),
+#          sample_date = parse_date_time(sample_date,
+#                                        tz = "EST",
+#                                        order = c("Y/m/d",
+#                                                  "B d Y",
+#                                                  "d B Y")),
+#          recaptured = str_to_lower(recaptured) %>% 
+#            str_sub(start = 1L,
+#                    end = 1L)
+#   )
+
+pacman::p_load(tidyverse, 
+               patchwork,
+               janitor,
+               palmerpenguins,
+               here)
+
+## check data entries before you analyze data
+
+## check class of each column
+## sapply and class
+sapply(df_messy,
+       FUN = class)
+
+## check unique elementsin each column
+## sapply and unique
+sapply(df_messy,
+       FUN = unique)
+
+## check possible type
+## stringdistmatrix()
+collector <- unique(df_messy$collector)
+stringdistmatrix(collector)
+
+
+# text cleaning in R ------------------------------------------------------
+
+## remove white space
+## str_squish
+a <- c(" a ", "a  b", "b ", "a", "b")
+str_squish(a)
+
+## align text case
+## str_to_lower() & str_to_upper
+b<- c("A", "a", "bB", "BB")
+str_to_lower(b)
+str_to_upper(b)
+
+## replace text
+## str_replace() & str_replace_all()
+
+v <- c("a b", "a.b", "a b.c")
+str_replace(v, "\\s", "_") %>%
+  str_replace("\\.", "_")
+
+str_replace_all(v, "\\s|\\.", "_")
+
+## remove text
+## str_remove() & str_remove_all()
+
+x <- c("abc", "dd", "abd")
+str_remove_all(x, "ab")
+
+## extract text
+z <- c("abc", "dd", "abd")
+str_extract_all(z, "ab")
+
+## detect text
+## str_detect()
+
+str_detect(z, "a")
+ifelse(str_detect(z, "a"),
+       yes = 1,
+       no = 0)
+
+y <- c("ab", "Ab")
+str_detect(y, "^a")
+str_detect(y,"^[Yy]" )
+
+
+# date object -------------------------------------------------------------
+
+d <- c("2024/06/01", "June 4 2024", "2024.06.07")
+lubridate::parse_date_time(d,
+                           tz = "EST",
+                           orders = c("Y/m/d",
+                           "B d Y",
+                           "Y.m.d"))
+
+
+# clean data --------------------------------------------------------------
+df_messy %>%
+  mutate(
+    species = species %>%
+      str_to_lower() %>%
+      str_squish() %>%
+      str_replace_all("\\s|\\.", "_") %>%
+      str_remove("_$"),
+    
+    length_mm = length_mm %>%
+      str_replace(",", ".") %>%
+      str_remove("\\smm") %>%
+      as.numeric(),
+    
+    sample_date = parse_date_time(
+      sample_date,
+      tz = "EST",
+      orders = c("Y/m/d",
+                 "B d Y",
+                 "d B Y",
+                 "Y.m.d")
+    ) %>% 
+      as.Date(),    # ← this must stay inside mutate()
+    
+    recaptured = ifelse(
+      str_detect(recaptured, "^[Yy]"),
+      yes = 1,
+      no = 0
+    )
+  )
+
+
+# column manipulation based on column type --------------------------------
+## mutate(), across(), where()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
